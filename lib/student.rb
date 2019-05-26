0f83afb6ff17bbdfd074946976f69780047e4c6e
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-26 01:39:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,10 +63,7 @@ class Student
   end 
   
   def self.new_from_db(row) 
-    binding.pry
-    @id = row[0]
-    @name = row[1]
-    @grade = row[2]
+    student = Student.new(row[0], row[1], row[2])
   end 
   
   def self.find_by_name 
